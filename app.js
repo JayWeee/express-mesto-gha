4e884373ab59eebd7545117321d940e85b644b6f
JayWee <tylykin2013@yandex.ru>
2023-07-09 10:38:05 +0300

    Добавить корневой роутер

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const usersRouter = require('./routes/users');
-const cardsRouter = require('./routes/cards');
+const { router } = require('./routes');
 
 const app = express();
 const { PORT = 3000 } = process.env;
@@ -16,11 +15,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/users', usersRouter);
-app.use('/cards', cardsRouter);
-
-app.use('/', (req, res) => {
-  res.status(404).send({ message: 'Такого пути не существует.' });
-});
+app.use(router);
 
 app.listen(PORT);
